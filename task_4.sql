96f4d6be5e0b23437ad4b84affda9b33853f8839
Robson Taye <ragnarrobie@gmail.com>
2025-08-18 00:09:16 +0300

    3

---
+++ task_4.sql
@@ -0,0 +1,14 @@
+-- Print the full description of the table 'books'
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_NAME = 'Books'
+ORDER BY 
+    ORDINAL_POSITION;
